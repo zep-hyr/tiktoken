002dbd1566573624d4dc556b8a56e5b1fb2070c7
zep.hyr <110882589+zep-hyr@users.noreply.github.com>
2025-06-26 21:06:40 +0900

    Update model.py

--- tiktoken/model.py
+++ tiktoken/model.py
@@ -7,7 +7,9 @@ from .registry import get_encoding
 MODEL_PREFIX_TO_ENCODING: dict[str, str] = {
     "o1-": "o200k_base",
     "o3-": "o200k_base",
+    "o4-": "o200k_base",
     # chat
+    "gpt-4.1-": "o200k_base",
     "chatgpt-4o-": "o200k_base",
     "gpt-4o-": "o200k_base",  # e.g., gpt-4o-2024-05-13
     "gpt-4-": "cl100k_base",  # e.g., gpt-4-0314, etc., plus gpt-4-32k
@@ -25,7 +27,9 @@ MODEL_TO_ENCODING: dict[str, str] = {
     # reasoning
     "o1": "o200k_base",
     "o3": "o200k_base",
+    "o4": "o200k_base",
     # chat
+    "gpt-4.1": "o200k_base",
     "gpt-4o": "o200k_base",
     "gpt-4": "cl100k_base",
     "gpt-3.5-turbo": "cl100k_base",
